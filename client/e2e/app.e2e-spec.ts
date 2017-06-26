b349337514af6556721a188d171947f103938330
MichaelGamel <michael.gamell2012@gmail.com>
2017-06-26 11:12:17 +0200

    Initial Startup Files

---
+++ client/e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { ClientPage } from './app.po';
+
+describe('client App', () => {
+  let page: ClientPage;
+
+  beforeEach(() => {
+    page = new ClientPage();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
