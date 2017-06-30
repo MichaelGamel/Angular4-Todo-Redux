485bf5b002545e17336c8e10030ec8d10a123857
MichaelGamel <michael.gamell2012@gmail.com>
2017-06-30 13:17:35 +0200

    Upgrade Packages

--- client/src/app/todos/add-todo/add-todo.component.ts
+++ client/src/app/todos/add-todo/add-todo.component.ts
@@ -1,9 +1,23 @@
+import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.css']
+  styleUrls: ['./add-todo.component.css'],
+  animations: [
+    trigger('explainerAnim', [
+      transition('* => *', [
+        query('.input-group', style({ opacity: 0, transform: 'translateX(-40px)' })),
+        query('.input-group', stagger('500ms', [
+          animate('500ms 1s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+        ])),
+        query('.input-group', [
+          animate(1000, style('*'))
+        ])
+      ])
+    ])
+  ]
 })
 export class AddTodoComponent implements OnInit {
 
--- client/src/app/todos/list-todo/list-todo.component.ts
+++ client/src/app/todos/list-todo/list-todo.component.ts
@@ -1,9 +1,26 @@
 import { ITodo } from './../../shared/models/ITodo.interface';
 import { Component, Input } from '@angular/core';
+import { trigger, style, transition, keyframes, animate, query, stagger } from '@angular/animations';
+
 @Component({
   selector: 'app-list-todo',
   templateUrl: './list-todo.component.html',
-  styleUrls: ['./list-todo.component.css']
+  styleUrls: ['./list-todo.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({ opacity: 0, transform: 'translateY(-75px)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: 0.3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+          ]))
+        ]), { optional: true })
+
+      ])
+    ])
+  ]
 })
 export class ListTodoComponent {
 
