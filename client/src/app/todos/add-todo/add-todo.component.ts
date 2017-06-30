fae30ba916f8dc2672a8c0cc6a2ce5ea7a979f0e
MichaelGamel <michael.gamell2012@gmail.com>
2017-06-30 13:20:02 +0200

    no message

--- client/src/app/todos/add-todo/add-todo.component.ts
+++ client/src/app/todos/add-todo/add-todo.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
       transition('* => *', [
         query('.input-group', style({ opacity: 0, transform: 'translateX(-40px)' })),
         query('.input-group', stagger('500ms', [
-          animate('500ms 1s ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+          animate('500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
         ])),
         query('.input-group', [
           animate(1000, style('*'))
